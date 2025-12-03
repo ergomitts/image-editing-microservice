25f99ed1fb8a91b49360c3d1b90bc3ff91692bdb
ergomitts <120071821+ergomitts@users.noreply.github.com>
2025-12-02 18:35:33 -0800

    Uploading Files

---
+++ client.py
@@ -0,0 +1,33 @@
+import zmq 
+import json 
+import os
+
+PORT = 1738
+
+
+
+def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect(f"tcp://localhost:{PORT}")
+    socket.send_json({
+        "command": "load",
+        "path": "photo.jpg"
+    })
+    response = socket.recv_json()
+    print(f"Received reply: {response}") 
+    socket.send_json({
+        "command": "resize",
+        "width": "500",
+        "height": "500"
+    })
+    response = socket.recv_json()
+    print(f"Received reply: {response}") 
+    socket.send_json({
+        "command": "monochrome"
+    })
+    response = socket.recv_json()
+    print(f"Received reply: {response}") 
+
+if __name__ == "__main__":
+    main()
